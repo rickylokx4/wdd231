e8db9c01c9962e102d260ed18eea49c4bc139012
rickylokx4 <jsalazarescobedo@byupathway.edu>
2025-01-21 16:17:02 +0100

    weather page update

---
+++ scripts/weather.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = '//api.openweathermap.org/data/2.5/weather?lat=49.75085963243803&lon=6.63678752162981&appid=e60b9c45685a0815605f08d9a36c7013&units=imperial';
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+   
+  console.log()
+
+  function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+  }
+
+  apiFetch();
+
