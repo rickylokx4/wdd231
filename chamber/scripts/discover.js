88e3913576325f411d6b689c717da385edc973a7
rickylokx4 <jsalazarescobedo@byupathway.edu>
2025-02-07 19:47:09 +0100

    update

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,6 +1,5 @@
 import { getCurrentYear, getLastModified } from "./functions.mjs";
 import { places } from "./places.mjs";
-console.log(places)
 
 getCurrentYear();
 getLastModified();
@@ -15,19 +14,16 @@ hamButton.addEventListener('click', () => {
 });
 
 // creating cards
-
 const cards = document.querySelector('#discover-cards')
 
 function makeCards(places){
     places.forEach(place => {
-
         const card = document.createElement('div')
         const title = document.createElement('h3')
         const image = document.createElement('img')
         const description = document.createElement('p')
         const address = document.createElement('address')
         const button = document.createElement('button')
-
         card.setAttribute("class", "discover-card")
         title.innerHTML = place.name;
         image.setAttribute("src", place.photo_url);
@@ -36,18 +32,29 @@ function makeCards(places){
         description.innerHTML = `Description: ${place.description} `;
         address.innerHTML = `Address: ${place.address} `;
         button.innerHTML = "Learn more"
-        
         card.appendChild(image)
         card.appendChild(title)
         card.appendChild(address)
         card.appendChild(description)
         card.appendChild(button)
         cards.appendChild(card)
-
-        
     });
-
-
 }
 makeCards(places)
+//last visit section
+const msToDays = 86400000;
+const lastVisitDiv = document.querySelector('#lastVisit')
 
+let lastVisit = Number(window.localStorage.getItem("lastVisit-ls")) || 0;
+let currentVisit = Date.now()
+let daysNotVisit = (currentVisit-lastVisit) / msToDays
+if(lastVisit == 0){
+    lastVisitDiv.innerHTML = `Welcome! Let us know if you have any questions.`
+}
+else if(daysNotVisit < 1){
+    lastVisitDiv.innerHTML = `Back so soon! Awesome!`
+}
+else if (daysNotVisit > 1){
+    lastVisitDiv.innerHTML = `You last visited ${daysNotVisit.toFixed(0)} days ago.`
+}
+localStorage.setItem("lastVisit-ls", currentVisit)
