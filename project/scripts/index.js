b4287c3713e269cfdd96ce0451bd69e19d182f44
rickylokx4 <jsalazarescobedo@byupathway.edu>
2025-02-08 19:21:33 +0100

    update

---
+++ project/scripts/index.js
@@ -0,0 +1,7 @@
+const navButton = document.querySelector('#navButton');
+const menuLinks = document.querySelector('#navBar');
+
+navButton.addEventListener('click', ()=>{
+    menuLinks.classList.toggle('open');
+    navButton.classList.toggle('open');
+});
