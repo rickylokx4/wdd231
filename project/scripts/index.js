1186dfee5b7e8fa6604a10da5e9ddada32ddfdaa
rickylokx4 <jsalazarescobedo@byupathway.edu>
2025-02-14 14:45:26 +0100

    update

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -12,19 +12,5 @@ navButton.addEventListener('click', ()=>{
     navButton.classList.toggle('open');
 });
 
-let sliderContainer = document.querySelector('.slider-container');
-let imagequantity = document.querySelectorAll('.slider-container img')
-let index = 1;
-
-setInterval(function(){
-    let percentage = index * -100;
-    sliderContainer.style.transform = "translateX("+percentage+"%)";
-    index++;
-    if(index > imagequantity.length-1){
-        index = 0;
-    }
-    
-},1000)
 
 
- 
