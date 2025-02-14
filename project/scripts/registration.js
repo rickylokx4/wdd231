e31739fb195b6c35c227f640a10efb290036ec2a
rickylokx4 <jsalazarescobedo@byupathway.edu>
2025-02-15 00:56:16 +0100

    update

---
+++ project/scripts/registration.js
@@ -0,0 +1,84 @@
+import { getCurrentYear, getLastModified} from "./functions.mjs";
+
+getCurrentYear()
+getLastModified()
+
+const navButton = document.querySelector('#navButton');
+const menuLinks = document.querySelector('#navBar');
+
+navButton.addEventListener('click', ()=>{
+    menuLinks.classList.toggle('open');
+    navButton.classList.toggle('open');
+});
+// config form age + 15
+document.getElementById("my-date").max = "2010-01-01";
+
+//timestamp
+
+const timestamp = new Date().toLocaleString();
+console.log(timestamp)
+document.querySelector('#timestamp').value = timestamp;
+
+// geting info from api (local json)
+
+const url = 'scripts/games.json'
+const infoContainer = document.querySelector('#games-cards')
+
+async function getGamesInfo() {
+	const response = await fetch(url);
+	const data = await response.json();
+	// console.log(data.games)
+    displayGames(data.games)
+    displayGames(data.games1)
+}
+
+const displayGames = (games) => {
+    const card = document.createElement('section')
+    games.forEach((game) =>{
+        //creating img 
+        
+        const img = document.createElement('img')
+        const button = document.createElement('button')
+        //creatin dialog
+        const dialog = document.createElement('dialog')
+		const dialogDiv = document.createElement('div')
+		const dialogTitle = document.createElement('h2')
+		const dialogButton = document.createElement('button')
+		const dialogParagraph = document.createElement('p')
+        //imagen settings
+        card.setAttribute("class", "card")
+        img.setAttribute("src", `${game.imageurl}`)
+        img.setAttribute("alt", `${game.name} poster`)
+        img.setAttribute("loanding", "lazy")
+        // button content
+        button.innerText = "See Details"
+        //adding cards content
+        dialogDiv.appendChild(dialogTitle)
+        card.appendChild(img)
+        infoContainer.appendChild(card)
+        dialogDiv.appendChild(dialogButton)
+        dialog.appendChild(dialogDiv)
+        dialog.appendChild(dialogParagraph)
+        infoContainer.appendChild(dialog)
+        dialogButton.addEventListener('click', ()=> {dialog.close()})
+        img.addEventListener('click', ()=> {showContent(game)})
+
+        function showContent(data){
+			dialogTitle.innerHTML = `${data.name} Tournament`
+			dialogButton.innerHTML = "&#10006;"
+			dialogParagraph.innerHTML = `
+			<p><span class="dialog-text">First Place:</span> ${data.firstPlace}</p>
+			<p><span class="dialog-text">Second Place:</span> ${data.secondPlace}</p>
+			<p><span class="dialog-text">Third Place:</span> ${data.thirdPlace}</p>
+			<p><span class="dialog-text">Top 10 prizes:</span> ${data.top10}</p>
+			`
+            dialog.showModal()
+		}
+
+
+
+
+    })};
+
+
+getGamesInfo()
---
+++ project/scripts/thanks.js
@@ -0,0 +1,43 @@
+import { getCurrentYear, getLastModified} from "./functions.mjs";
+
+getCurrentYear()
+getLastModified()
+
+// Nav Button Config stars
+const navButton = document.querySelector('#navButton');
+const menuLinks = document.querySelector('#navBar');
+
+navButton.addEventListener('click', ()=>{
+    menuLinks.classList.toggle('open');
+    navButton.classList.toggle('open');
+});
+
+
+//Geting data from the form
+const currrentUrl = window.location.href;
+const everything = currrentUrl.split('?')
+let formData = everything[1].split('&')
+console.log(formData)
+let result
+function show(info){
+    formData.forEach((element) => {
+        if(element.startsWith(info)) {
+            result = element.split('=')[1].replace("%40","@").replaceAll("%2F","/").replace("%2C+"," ").replace("%2B","").replaceAll("%3A",":").replaceAll("+"," ")
+        }
+    })
+    return (result)
+}
+const info = document.querySelector('#information')
+info.innerHTML = `
+
+<h2 id ="form-h2">Registration Info</h2>
+<p><span class="form-data">Gamer Name: </span>${show("fname")} ${show("lname")}</p>
+<p><span class="form-data">Gamer NickName: </span>${show("nickname")}</p>
+<p><span class="form-data">Email: </span><a href="${show("email")}">${show("email")}</a></p>
+<p><span class="form-data">Phone:</span> ${show("phone")}</p>
+<p><span class="form-data">Birthday: </span>${show("birthday")}</p>
+<p><span class="form-data">Game:</span> ${show("game")}</p>
+<p><span class="form-data">Registration date and time: </span>${show("timestamp")}</p>
+
+
+`
