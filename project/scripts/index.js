ebbef9706d518e3752c3df19d7aa5de689d21697
rickylokx4 <jsalazarescobedo@byupathway.edu>
2025-02-16 00:51:09 +0100

    update

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -11,6 +11,25 @@ navButton.addEventListener('click', ()=>{
     menuLinks.classList.toggle('open');
     navButton.classList.toggle('open');
 });
+// setting Welcome
+const msToDays = 86400000;
+const welcomeMessage = document.querySelector('#welcome-message')
 
+let lastVisit = Number(window.localStorage.getItem("lastVisit-ls")) || 0;
+let currentVisit = Date.now()
+let daysNotVisit = (currentVisit-lastVisit) / msToDays
+if(lastVisit == 0){
+    welcomeMessage.innerHTML = `Welcome, come and have fun with us!! `
+}
+else if(daysNotVisit < 1){
+    welcomeMessage.innerHTML = `Back so soon! It's obvious that you like fighting games!!`
+}
+else if (daysNotVisit > 1 & daysNotVisit < 2){
+    welcomeMessage.innerHTML = `Your last visit was yesterday, don't forget to register!!`
+}
+else{
+    welcomeMessage.innerHTML = `You last visited ${daysNotVisit.toFixed(0)} days ago, You still have time to register`
+}
+localStorage.setItem("lastVisit-ls", currentVisit)
 
 
